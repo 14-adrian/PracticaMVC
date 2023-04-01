075bb55ddea302e87a4155bf485bd07cbe8ab08f
Leoni <Leoni@WARMACHINE>
2023-04-01 00:43:41 -0600

    PracticaMVC cont

--- PracticaMVC/Models/equiposDbContext.cs
+++ PracticaMVC/Models/equiposDbContext.cs
@@ -7,6 +7,12 @@ namespace PracticaMVC.Models
        public equiposDbContext(DbContextOptions<equiposDbContext> options) : base(options)
        { 
        }
+        public DbSet<carreras> carreras { get; set; }
+        public DbSet<estados_equipo> estados_equipo { get; set; }
+        public DbSet<estados_reserva> estados_reserva { get; set; }
+        public DbSet<facultades> facultades { get; set; }
         public DbSet<marcas> marcas { get; set; }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+        public DbSet<usuarios> usuarios { get; set; }
     }
 }
